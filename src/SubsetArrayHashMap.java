b93195c082e5d3c0c724d211a29c011e723902b7
swathy-r <swathyranganathan2003@gmail.com>
2025-03-09 23:40:22 +0530

    new file added

---
+++ src/SubsetArrayHashMap.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+public class SubsetArrayHashMap {
+    public static void main(String[] args) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter the number of test cases: ");
+        int t = Integer.parseInt(read.readLine().trim());
+        while (t-- > 0) {
+            System.out.print("Enter elements of array a (space-separated): ");
+            String line = read.readLine().trim();
+            String[] numsStr = line.split(" ");
+            int[] a = new int[numsStr.length];
+            for (int i = 0; i < numsStr.length; i++) {
+                a[i] = Integer.parseInt(numsStr[i]);
+            }
+            System.out.print("Enter elements of array b (space-separated): ");
+            line = read.readLine().trim();
+            numsStr = line.split(" ");
+            int[] b = new int[numsStr.length];
+            for (int i = 0; i < numsStr.length; i++) {
+                b[i] = Integer.parseInt(numsStr[i]);
+            }
+            Subset ob = new Subset();
+            if (ob.isSubset(a, b))
+                System.out.println("true");
+            else
+                System.out.println("false");
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Subset {
+    public boolean isSubset(int a[], int b[]) {
+        // Your code here
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<a.length;i++){
+            if(map.containsKey(a[i])){
+                map.put(a[i],map.get(a[i])+1);
+            }
+            else{
+                map.put(a[i],1);
+            }
+        }
+        for(int j=0;j<b.length;j++){
+            if(!map.containsKey(b[j]) || map.get(b[j])==0){
+                return false;
+            }
+            map.put(b[j],map.get(b[j])-1);
+        }
+        return true;
+    }
+}
